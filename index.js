9170ab27ed4de7229a62daacdf4af1cde85d07c1
HippolyteWeber <hippolyte.weber@gmail.com>
2024-03-19 11:24:50 +0100

    Hello Wilders 1.0.1

---
+++ index.js
@@ -0,0 +1,12 @@
+import chalk from "chalk";
+
+console.log(
+  `hello`,
+  chalk.red(`Angélique`),
+  `hello`,
+  chalk.magenta(`Pierre`),
+  `hello`,
+  chalk.cyan(`Yann`),
+  `hello`,
+  chalk.greenBright(`Zainouddine`)
+);
